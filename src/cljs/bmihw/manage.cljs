c694e5033f953f50840829f4d4e11250cd90f8a5
Daniel Rugg <dfrugg@gmail.com>
2015-08-29 04:16:43 -0400

    Cleaning up styling on manage

--- src/cljs/bmihw/manage.cljs
+++ src/cljs/bmihw/manage.cljs
@@ -41,52 +41,60 @@
 (defn delete-page
   []
   (if-let [drunk (:drunk @current-event)]
-    [:div 
-     [:p "NOPE!!!"]
-     [:p "Always do sober what you said you'd do drunk. That will teach you to keep your mouth shut."]
-     [:i "- Ernest Hemmingway"]
-     [:br]
-     [:br]
-     [:div [:a {:href "#/manage"
-                :on-click (fn [e] 
-                            (reset! current-event nil)
-                            (session/put! :current-page #'manage-page))} "Back To Manage Submissions"]]]
-    [:div
-     [:p "Are you sure you want to hide the evidence?"]
-     [:a {:href "#/manage"
-                :on-click (fn [e]
-                            (let [id (:id @current-event)
-                                  ref (.child fb id)
-                                  clean-events (filter (fn [e] (not (= id (:id e))))  @events)]
-                              (.remove ref)
-                              (reset! events clean-events)
-                              (reset! current-event nil)
-                              (session/put! :current-page #'manage-page)))} "Yes, I'm a pussy."]
-     [:a {:href "#/manage"
-                :on-click (fn [e] 
-                            (reset! current-event nil)
-                            (session/put! :current-page #'manage-page))} "Hell No!."]]))
+    [:div.panel.panel-default 
+     [:div.panel-heading 
+      [:h3.panel-title "NOPE!!!!"]]
+     [:div.panel-body
+	     [:p "Always do sober what you said you'd do drunk. That will teach you to keep your mouth shut."]
+	     [:i "- Ernest Hemmingway"]
+	     [:br]
+	     [:br]
+	     [:div.btn-group.btn-group-justified
+	      [:a.btn.btn-default {:href "#/manage"
+	           :on-click (fn [e] 
+	                       (reset! current-event nil)
+	                       (session/put! :current-page #'manage-page))} "Back To Manage Submissions"]]]]
+    [:div.panel.panel-default
+     [:div.panel-heading 
+      [:h3.panel-title "Really???"]]
+     [:div.panel-body
+	     [:p "Are you sure you want to hide the evidence?"]
+	     [:div.btn-group.btn-group-justified
+		     [:a.btn.btn-default {:href "#/manage"
+	           :on-click (fn [e]
+		                      (let [id (:id @current-event)
+		                            ref (.child fb id)
+		                            clean-events (filter (fn [e] (not (= id (:id e))))  @events)]
+		                        (.remove ref)
+		                        (reset! events clean-events)
+		                        (reset! current-event nil)
+		                        (session/put! :current-page #'manage-page)))} "Yes, I'm a pussy."]
+		     [:a.btn.btn-default {:href "#/manage"
+	           :on-click (fn [e] 
+	                       (reset! current-event nil)
+	                       (session/put! :current-page #'manage-page))} "Hell No!."]]]]))
   
 
 (defn manage-page
   []
   (let [who (get-in auth ["uid"])]
 	  [:div.panel.panel-default 
-     [:div.panel-heading "Manage Your Insults To Those Deserving Bastards!"]
-	   [:table#manage {:style {:width "100%"}}
+     [:div.panel-heading 
+      [:h3.panel-title "Manage Your Insults To Those Deserving Bastards!"]]
+     [:div.panel-body
+	   [:table#manage.table {:style {:width "100%"}}
 	    [:tr
 	     [:th "Drunk"][:th "Event"][:th "Insult"][:th "Keyword"] [:td who]]
      (for [event @events]
        [:tr {:key (:id event)}
-        [:td (or (and (:drunk event) "Hell Yeah!") "Nope")]
+        [:td (or (and (:drunk event) "Yep") "Nope")]
         [:td (:target event)]
         [:td (:content event)]
         [:td (:keyword event)]
-        [:td [:input {:type "button" 
-                      :value "Delete" 
-                      :on-click (fn [e] 
-                                  (reset! current-event event)
-                                  (session/put! :current-page #'delete-page))}]]])]]))
+        [:td [:button.btn.btn-default {:type "button" 
+                                       :on-click (fn [e] 
+                                                   (reset! current-event event)
+                                                   (session/put! :current-page #'delete-page))} "Delete"]]])]]]))
 
 (.on fb "child_added" handle-childsnapshot)
 
