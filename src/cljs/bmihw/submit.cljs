0192153d951bfd367496bf08fac896b92f4937f5
oakes <zsoakes@gmail.com>
2015-08-29 00:17:41 -0400

    SUBMIT TO FIREBASE

--- src/cljs/bmihw/submit.cljs
+++ src/cljs/bmihw/submit.cljs
@@ -1,17 +1,26 @@
 (ns bmihw.submit
-  (:require [bmihw.common :refer [fb]]))
+  (:require [bmihw.common :refer [auth fb]]
+            [reagent-forms.core :refer [bind-fields]]))
 
-(defn submit-to-fb []
-  (.log js/console fb))
+(defn submit-to-fb [state]
+  (.push fb
+    (clj->js state)
+    (fn [error]
+      (when error
+        (js/alert (str "An error occurred: " error))))))
 
 (defn submit-page []
-  [:div [:h2 "Submit your insult."]
-   [:div [:div
-          [:input {:type :text :name :username :placeholder "Username"}]
-          [:br]
-          [:input {:type :text :name :keyword :placeholder "Keyword"}]
-          [:br]
-          [:input {:type :text :name :content :placeholder "Content"}]
-          [:br]
-          [:input {:type :submit :value "Submit"
-                   :on-click submit-to-fb}]]]])
+  (let [state (atom {})]
+    [:div [:h2 "Submit your insult."]
+     [:div [:div
+            [:input {:type :text :name :username :placeholder "Username"
+                     :on-change #(swap! state assoc :username (-> % .-target .-value))}]
+            [:br]
+            [:input {:type :text :name :keyword :placeholder "Keyword"
+                     :on-change #(swap! state assoc :keyword (-> % .-target .-value))}]
+            [:br]
+            [:input {:type :text :name :content :placeholder "Content"
+                     :on-change #(swap! state assoc :content (-> % .-target .-value))}]
+            [:br]
+            [:input {:type :submit :value "Submit"
+                     :on-click #(submit-to-fb @state)}]]]]))
